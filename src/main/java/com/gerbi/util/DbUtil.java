db23f341b2f817dab4d9505cba5a5f948cb4bb0a
Strelia <strelchenko.vitaliy.95@gamil.com>
2016-03-01 01:39:25 +0200

    Project finish 1

--- src/main/java/com/gerbi/util/DbUtil.java
+++ src/main/java/com/gerbi/util/DbUtil.java
@@ -5,29 +5,42 @@
  */
 package com.gerbi.util;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DbUtil {
-     private static Connection connection = null;
+    private static Connection connection = null;
 
 
     public static Connection getConnection() {
         if (connection == null) {
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                String url = "jdbc:mysql://localhost:3306/notebook";
-                String username = "root";
-                String password = "root";
-                connection = DriverManager.getConnection(url, username, password);
+                Properties prop = new Properties();
+                InputStream inputStream = DbUtil.class.getClassLoader().getResourceAsStream("/db.properties");
+                prop.load(inputStream);
+
+                Class.forName(prop.getProperty("driver"));
+                String url = prop.getProperty("url");
+                String username = prop.getProperty("username");
+                String password = prop.getProperty("password");
+                if(username.isEmpty() || password.isEmpty()) {
+                    connection = DriverManager.getConnection(url);
+                }else {
+                    connection = DriverManager.getConnection(url, username, password);
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             } catch (ClassNotFoundException e) {
                 e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
             }
             return connection;
-        }else {
+        } else {
             return connection;
         }
     }
